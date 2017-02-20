e0d5fc76ea67fb70f33845badedfa6bf72a8a419
niyathisoftware <niyathisoftware@gmail.com>
2017-02-20 13:37:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,19 +8,19 @@ app.use(morgan('combined'));
 
 var articles = {
     
-    article-one  {
+    articleone : {
     title : "Ram Test1",
     heading : "Heading1",
     date: "Feb 20, 2017",
     content : "This is Ram Kris1"},
     
-    article-two : {
+    articletwo : {
     title : "Ram Test2",
     heading : "Headin2g",
     date: "Feb 20, 2017",
     content : "This is Ram Kris2"},
     
-    article-three : {
+    articlethree : {
     title : "Ram Test3",
     heading : "Heading3",
     date: "Feb 20, 2017",
@@ -61,7 +61,7 @@ var htmlTemplate = `
 </html>
 `;
  return htmlTemplate;
-};
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -69,29 +69,9 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
    var articleName = req.param.articleName;
-  res.send(createTemplate(articles[:articleName]));
+  res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
-
-app.get('/article-one', function (req, res) {
-  res.send('article one is requested and served here');
-});
-
-app.get('/article-two', function (req, res) {
-  res.send('article two is requested and served here');
-});
-
-app.get('/article-three', function (req, res) {
-  res.send('article three is requested and served here');
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
