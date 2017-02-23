e6564d7e87b32d47ec876d4fe27ec27a887bc3e1
niyathisoftware <niyathisoftware@gmail.com>
2017-02-23 14:46:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
     res.sendFile(path.join(_dirname, 'ui', 'main.js'));
-    };
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
