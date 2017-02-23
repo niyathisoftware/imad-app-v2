b47969865f7cf68ecd8c8a902726b1e03e6c077c
niyathisoftware <niyathisoftware@gmail.com>
2017-02-23 15:10:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML= 'New Value';
+
+var img = document.getElementById('madi');
+
+img.onClick= function () {
+    img.Style.marginLeft = '100px'
+};
