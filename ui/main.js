531879840418f5e57ea48e913a07f3e40b1c9368
niyathisoftware <niyathisoftware@gmail.com>
2017-02-28 16:17:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,21 @@ console.log('Loaded!');
 var element = document.getElementById('counter');
 var counter = 0;
 button.onClick = function () {
-    counter = counter + 1;
-    var span - document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+        if (request.readystate === XMLHttpRequest.Done ) {
+            if (request.status === 200)
+            var counter = request.responseText;
+            var span - document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    };
+
+    
+  
+    
 };
 
 var element = document.getElementById('main-text');
