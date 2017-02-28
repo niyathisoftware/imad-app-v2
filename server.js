866d422ae2befc877a16ee3176703a52151c2002
niyathisoftware <niyathisoftware@gmail.com>
2017-02-28 15:18:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
-    res.send(toString());
+    res.send(counter,toString());
 });
     
 
