d18c8871c20d155687b72b36e5d1d222b0d126be
niyathisoftware <niyathisoftware@gmail.com>
2017-03-02 17:33:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+    host :'db.imad.hasura-app.io',
+    user : 'niyathisoftware',
+    password : 'process.env.DB_PASSWORD',
+    database : 'niyathisoftware',
+    port : '5432'
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -65,6 +72,18 @@ var htmlTemplate = `
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var Pool = new Pool(config);
+app.get('/niyathisoftware', function (req, res) {
+    post.query('SELECT * FROM test', function (err, result){
+        if (err) {
+            res.status('500').send(err,toString());
+        } else {
+            res.send(JSON.Stringigy(result));
+        }
+        
+    });
 });
 
 var counter = 0;
