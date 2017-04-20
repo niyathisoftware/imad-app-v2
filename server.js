44a5084afa732d7d047667298a3a4e33587f9066
niyathisoftware <niyathisoftware@gmail.com>
2017-04-20 12:54:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,10 +78,16 @@ app.get('/', function (req, res) {
 });
 
 function hash(input, salt) {
-    var hashed = crypto.pbkd2fSync(input, salt, 10000, 512. 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512,'sha512');
     return ['pbkdf2',10000, salt, hashed.toString('hex')].join('$');
     
 }
+
+app.get('/hash/:input', function (req, res) {
+    var hashedString = hash(req.param.input, 'this-is-some-random-string');
+    res.send(hashedString);
+    
+})
 
 
 var Pool = new Pool(config);
