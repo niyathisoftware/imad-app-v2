98425893ab8f79bc82da98c4a07a12ed20eee636
niyathisoftware <niyathisoftware@gmail.com>
2017-04-20 14:30:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,6 +105,23 @@ app.post('/create-user', function (req, res) {
 });
 
 
+app.post('/login', function(req, res) {
+    
+  var username = req.body.username;
+  var password = req.body.password;
+  pool.query('SELECT * FROM "user" username values ($1)', [username], function (err, result) {
+           if (err) {
+            res.status('500').send(err,toString());
+        } else {
+            if (result.rows.length === 0)
+            res.status('403').send('username/password is invalid');
+            
+        }        
+        }        
+  });
+});
+
+
 
 
 
